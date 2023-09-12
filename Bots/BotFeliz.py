cb9ae46d83bc86a771ea8e56349c4a96ecbacb60
Henrique1803 <henriquemateusteodoro@gmail.com>
2023-09-12 10:29:21 -0300

    atualizando integração com grupo 1

--- Bots/BotFeliz.py
+++ Bots/BotFeliz.py
@@ -1,30 +1,26 @@
 from Bots.Bot import Bot
 
 class BotFeliz(Bot):
-    def __init__(self,nome):
+    def __init__(self, nome):
         super().__init__(nome)
-        self.comandos = {"1" : 'Bom dia, flor do dia!', "2": 'Acredite em si mesmo e em seu potencial ilimitado. Você é mais capaz do que imagina!', "3": 'Seu esforço e dedicação são realmente admiráveis. Continue assim!', "4": self.despedida() }
-
+        
     def apresentacao(self):
-        return ("Olá! Eu sou o Risobótico e estou disposto a alegrar o seu dia!")
-
-    def mostra_comandos(self):
-        return("1 - Bom dia\n2 - Mensagem motivacional\n3 - Elogios\n4 - Despedida")
-
-
-
+        return f'Olá! Meu nomé é {self.nome}, como você está?'
+    
+    def boas_vindas(self):
+        return 'Ebaaa! Feliz que você me escolheuuu :)'  
     
     def executa_comando(self,cmd):
-        return self.comandos[cmd]
-        
-
-
-
-    def boas_vindas(self):
-    
-        return ("Que bom que você me escolheu! Vamos ter uma conversa muito agradável!") 
-
-
+        if cmd == "1":
+            return 'Bem-vindo ao chat! Espero que esteja bem'
+        elif cmd== "2":
+            return f'Olá! Meu nomé é {self.nome}, como você está?'
+        elif cmd== "3":
+            return 'Veja a vida de forma mais positiva!'        
+        elif cmd=="-1" or cmd == "4":
+            return self.despedida()
 
     def despedida(self):
-        return ("É uma pena que você já vai embora :( , foi um prazer ter animado você!")
+        return 'Até outra hora! Espero te ver mais uma vez!'
+
+        
---
+++ Bots/BotFelizinho.py
@@ -0,0 +1,22 @@
+from Bots.Bot import Bot
+
+class BotFelizinho(Bot):
+    def __init__(self,nome):
+        super().__init__(nome)
+        self.comandos = {"1" : 'Bom dia, flor do dia!', "2": 'Acredite em si mesmo e em seu potencial ilimitado. Você é mais capaz do que imagina!', "3": 'Seu esforço e dedicação são realmente admiráveis. Continue assim!', "4": self.despedida() }
+
+    def apresentacao(self):
+        return ("Olá! Eu sou o Risobótico e estou disposto a alegrar o seu dia!")
+
+    def mostra_comandos(self):
+        return("1 - Bom dia\n2 - Mensagem motivacional\n3 - Elogios\n4 - Despedida")
+
+    def executa_comando(self,cmd):
+        return self.comandos[cmd]
+        
+    def boas_vindas(self):
+    
+        return ("Que bom que você me escolheu! Vamos ter uma conversa muito agradável!") 
+
+    def despedida(self):
+        return ("É uma pena que você já vai embora :( , foi um prazer ter animado você!")
