2a8d2eb03d99c362cf67280781aef940103219f8
Henrique1803 <henriquemateusteodoro@gmail.com>
2023-09-11 21:01:55 -0300

    inserindo novos bots

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -4,31 +4,51 @@ class SistemaChatBot:
     def __init__(self,nomeEmpresa,lista_bots):
         self.__empresa=nomeEmpresa
         ##verificar se a lista de bots contém apenas bots
-        self.__lista_bots=lista_bots
+        self.__lista_bots= []
+        for bot in lista_bots:
+            if isinstance(bot, Bot):
+                self.__lista_bots.append(bot)
         self.__bot = None
     
     def boas_vindas(self):
-        pass
-        ##mostra mensagem de boas vindas do sistema
+        print(f"Olá, esse é o Sistema de chatbots da empresa {self.__empresa}")
 
     def mostra_menu(self):
-        pass
+        print("Os chat bots disponíveis no momento são: ")
+        for i in range(len(self.__lista_bots)):
+            print(f"{i} - Bot: {self.__lista_bots[i].nome} - Mensagem de apresentação: {self.__lista_bots[i].apresentacao()}")
         ##mostra o menu de escolha de bots
     
     def escolhe_bot(self):
-        pass
+        num_escolhido = int(input("Digite o número do chat bot desejado: "))
         ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
+        self.__bot = self.__lista_bots[num_escolhido]
+        print(f"--> {self.__bot.nome} diz: {self.__bot.boas_vindas()}")
+
 
     def mostra_comandos_bot(self):
-        pass
+        print(self.__bot.mostra_comandos())
         ##mostra os comandos disponíveis no bot escolhido
 
     def le_envia_comando(self):
-        pass
+        while True:
+            self.mostra_comandos_bot()
+            comando = int(input("Digite o comando desejado (ou -1 para fechar): "))
+            if comando == -1:
+                self.__bot.despedida()
+                break
+            else:
+                print(self.__bot.executa_comando(comando))
+
+
+
         ##faz a entrada de dados do usuário e executa o comando no bot ativo
 
     def inicio(self):
-        pass
+        self.boas_vindas()
+        self.mostra_menu()
+        self.escolhe_bot()
+        self.le_envia_comando()
         ##mostra mensagem de boas-vindas do sistema
         ##mostra o menu ao usuário
         ##escolha do bot      
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
+from Bots.BotGago import BotGago
+from Bots.BotFeliz import BotFeliz
+from Bots.BotNaruto import BotNaruto
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotFeliz("Lucian"), BotGago("Elio"), BotNaruto("Naruto")]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
